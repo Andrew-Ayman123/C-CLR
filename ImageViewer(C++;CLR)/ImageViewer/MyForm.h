0bb15431cf64c84b1b3bea018e0c1cf8ebacb99f
Andrew-Ayman123 <88400418+Andrew-Ayman123@users.noreply.github.com>
2021-08-03 20:38:39 +0200

    Add files via upload

---
+++ ImageViewer(C++;CLR)/ImageViewer/MyForm.h
@@ -0,0 +1,380 @@
+﻿#pragma once
+
+namespace ImageViewer {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Panel^  panel1;
+	protected:
+	private: System::Windows::Forms::Panel^  panel2;
+	private: System::Windows::Forms::PictureBox^  pictureBox1;
+	private: System::Windows::Forms::Button^  button6;
+	private: System::Windows::Forms::Button^  button5;
+	private: System::Windows::Forms::Button^  button4;
+	private: System::Windows::Forms::Button^  button3;
+	private: System::Windows::Forms::Button^  button7;
+	private: System::Windows::Forms::Button^  button2;
+	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::OpenFileDialog^  openFileDialog1;
+	private: System::Windows::Forms::Button^  button8;
+	private: System::Windows::Forms::Button^  button9;
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->panel1 = (gcnew System::Windows::Forms::Panel());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->panel2 = (gcnew System::Windows::Forms::Panel());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->button6 = (gcnew System::Windows::Forms::Button());
+			this->button9 = (gcnew System::Windows::Forms::Button());
+			this->button8 = (gcnew System::Windows::Forms::Button());
+			this->button5 = (gcnew System::Windows::Forms::Button());
+			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->button7 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->openFileDialog1 = (gcnew System::Windows::Forms::OpenFileDialog());
+			this->panel1->SuspendLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			this->panel2->SuspendLayout();
+			this->SuspendLayout();
+			// 
+			// panel1
+			// 
+			this->panel1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+				| System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->panel1->Controls->Add(this->pictureBox1);
+			this->panel1->Location = System::Drawing::Point(0, 0);
+			this->panel1->Margin = System::Windows::Forms::Padding(3, 4, 3, 4);
+			this->panel1->Name = L"panel1";
+			this->panel1->Size = System::Drawing::Size(790, 340);
+			this->panel1->TabIndex = 0;
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+				| System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->pictureBox1->Location = System::Drawing::Point(14, 16);
+			this->pictureBox1->Margin = System::Windows::Forms::Padding(3, 4, 3, 4);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(762, 320);
+			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::Zoom;
+			this->pictureBox1->TabIndex = 0;
+			this->pictureBox1->TabStop = false;
+			// 
+			// panel2
+			// 
+			this->panel2->BackColor = System::Drawing::Color::Black;
+			this->panel2->Controls->Add(this->label1);
+			this->panel2->Controls->Add(this->button6);
+			this->panel2->Controls->Add(this->button9);
+			this->panel2->Controls->Add(this->button8);
+			this->panel2->Controls->Add(this->button5);
+			this->panel2->Controls->Add(this->button4);
+			this->panel2->Controls->Add(this->button3);
+			this->panel2->Controls->Add(this->button7);
+			this->panel2->Controls->Add(this->button2);
+			this->panel2->Controls->Add(this->button1);
+			this->panel2->Dock = System::Windows::Forms::DockStyle::Bottom;
+			this->panel2->Location = System::Drawing::Point(0, 348);
+			this->panel2->Margin = System::Windows::Forms::Padding(3, 4, 3, 4);
+			this->panel2->Name = L"panel2";
+			this->panel2->Size = System::Drawing::Size(790, 118);
+			this->panel2->TabIndex = 1;
+			// 
+			// label1
+			// 
+			this->label1->Anchor = System::Windows::Forms::AnchorStyles::None;
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Segoe UI", 15.75F));
+			this->label1->ForeColor = System::Drawing::Color::White;
+			this->label1->Location = System::Drawing::Point(263, 44);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(67, 30);
+			this->label1->TabIndex = 1;
+			this->label1->Text = L"Zoom";
+			// 
+			// button6
+			// 
+			this->button6->Anchor = System::Windows::Forms::AnchorStyles::Bottom;
+			this->button6->Font = (gcnew System::Drawing::Font(L"Segoe UI", 15.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button6->Location = System::Drawing::Point(346, 16);
+			this->button6->Margin = System::Windows::Forms::Padding(3, 4, 3, 4);
+			this->button6->Name = L"button6";
+			this->button6->Size = System::Drawing::Size(45, 85);
+			this->button6->TabIndex = 0;
+			this->button6->Text = L"-";
+			this->button6->UseVisualStyleBackColor = true;
+			this->button6->Click += gcnew System::EventHandler(this, &MyForm::button6_Click);
+			// 
+			// button9
+			// 
+			this->button9->Anchor = System::Windows::Forms::AnchorStyles::Bottom;
+			this->button9->Font = (gcnew System::Drawing::Font(L"Segoe UI", 10.75F));
+			this->button9->Location = System::Drawing::Point(447, 14);
+			this->button9->Margin = System::Windows::Forms::Padding(3, 4, 3, 4);
+			this->button9->Name = L"button9";
+			this->button9->Size = System::Drawing::Size(44, 39);
+			this->button9->TabIndex = 0;
+			this->button9->Text = L"Ro";
+			this->button9->UseVisualStyleBackColor = true;
+			this->button9->Click += gcnew System::EventHandler(this, &MyForm::button9_Click);
+			// 
+			// button8
+			// 
+			this->button8->Anchor = System::Windows::Forms::AnchorStyles::Bottom;
+			this->button8->Font = (gcnew System::Drawing::Font(L"Segoe UI", 15.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button8->Location = System::Drawing::Point(447, 61);
+			this->button8->Margin = System::Windows::Forms::Padding(3, 4, 3, 4);
+			this->button8->Name = L"button8";
+			this->button8->Size = System::Drawing::Size(44, 39);
+			this->button8->TabIndex = 0;
+			this->button8->Text = L"R";
+			this->button8->UseVisualStyleBackColor = true;
+			this->button8->Click += gcnew System::EventHandler(this, &MyForm::button8_Click);
+			// 
+			// button5
+			// 
+			this->button5->Anchor = System::Windows::Forms::AnchorStyles::Bottom;
+			this->button5->Font = (gcnew System::Drawing::Font(L"Segoe UI", 15.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button5->Location = System::Drawing::Point(397, 15);
+			this->button5->Margin = System::Windows::Forms::Padding(3, 4, 3, 4);
+			this->button5->Name = L"button5";
+			this->button5->Size = System::Drawing::Size(44, 85);
+			this->button5->TabIndex = 0;
+			this->button5->Text = L"+";
+			this->button5->UseVisualStyleBackColor = true;
+			this->button5->Click += gcnew System::EventHandler(this, &MyForm::button5_Click);
+			// 
+			// button4
+			// 
+			this->button4->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Bottom | System::Windows::Forms::AnchorStyles::Right));
+			this->button4->Font = (gcnew System::Drawing::Font(L"Segoe UI", 15.75F));
+			this->button4->Location = System::Drawing::Point(525, 17);
+			this->button4->Margin = System::Windows::Forms::Padding(3, 4, 3, 4);
+			this->button4->Name = L"button4";
+			this->button4->Size = System::Drawing::Size(113, 85);
+			this->button4->TabIndex = 0;
+			this->button4->Text = L"Close";
+			this->button4->UseVisualStyleBackColor = true;
+			this->button4->Click += gcnew System::EventHandler(this, &MyForm::button4_Click);
+			// 
+			// button3
+			// 
+			this->button3->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Bottom | System::Windows::Forms::AnchorStyles::Right));
+			this->button3->Font = (gcnew System::Drawing::Font(L"Segoe UI", 15.75F));
+			this->button3->Location = System::Drawing::Point(645, 17);
+			this->button3->Margin = System::Windows::Forms::Padding(3, 4, 3, 4);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(113, 85);
+			this->button3->TabIndex = 0;
+			this->button3->Text = L"Open";
+			this->button3->UseVisualStyleBackColor = true;
+			this->button3->Click += gcnew System::EventHandler(this, &MyForm::button3_Click);
+			// 
+			// button7
+			// 
+			this->button7->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Bottom | System::Windows::Forms::AnchorStyles::Left));
+			this->button7->Font = (gcnew System::Drawing::Font(L"Wingdings", 15.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(2)));
+			this->button7->Location = System::Drawing::Point(187, 17);
+			this->button7->Margin = System::Windows::Forms::Padding(3, 4, 3, 4);
+			this->button7->Name = L"button7";
+			this->button7->Size = System::Drawing::Size(57, 85);
+			this->button7->TabIndex = 0;
+			this->button7->Text = L"";
+			this->button7->UseVisualStyleBackColor = true;
+			this->button7->Click += gcnew System::EventHandler(this, &MyForm::button7_Click);
+			// 
+			// button2
+			// 
+			this->button2->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Bottom | System::Windows::Forms::AnchorStyles::Left));
+			this->button2->Font = (gcnew System::Drawing::Font(L"Wingdings", 15.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(2)));
+			this->button2->Location = System::Drawing::Point(122, 17);
+			this->button2->Margin = System::Windows::Forms::Padding(3, 4, 3, 4);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(57, 85);
+			this->button2->TabIndex = 0;
+			this->button2->Text = L"";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			// 
+			// button1
+			// 
+			this->button1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Bottom | System::Windows::Forms::AnchorStyles::Left));
+			this->button1->Font = (gcnew System::Drawing::Font(L"Segoe UI", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button1->Location = System::Drawing::Point(28, 17);
+			this->button1->Margin = System::Windows::Forms::Padding(3, 4, 3, 4);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(87, 85);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"Reset All";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// openFileDialog1
+			// 
+			this->openFileDialog1->FileName = L"ofd";
+			this->openFileDialog1->Filter = L"\"JPEG|*.jpg|All Files |*.*\"";
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(7, 17);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(790, 466);
+			this->Controls->Add(this->panel2);
+			this->Controls->Add(this->panel1);
+			this->Font = (gcnew System::Drawing::Font(L"Segoe UI", 9.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->Margin = System::Windows::Forms::Padding(3, 4, 3, 4);
+			this->MinimumSize = System::Drawing::Size(806, 505);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->panel1->ResumeLayout(false);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			this->panel2->ResumeLayout(false);
+			this->panel2->PerformLayout();
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+
+		array<String^>^ files;
+		String ^path;
+		int currentIndex;
+		const int zoomlevel = 50;
+		int zooms = 0;
+		int rotates;
+	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+		Application::Restart();
+	
+	}
+private: System::Void button4_Click(System::Object^  sender, System::EventArgs^  e) {
+	pictureBox1->ImageLocation = "";
+
+}
+private: System::Void button3_Click(System::Object^  sender, System::EventArgs^  e) {
+	if (openFileDialog1->ShowDialog() == System::Windows::Forms::DialogResult::OK) {
+		pictureBox1->ImageLocation = openFileDialog1->FileName;
+		path = System::IO::Path::GetDirectoryName(openFileDialog1->FileName);
+		files = System::IO::Directory::GetFiles(path);
+		for (int i = 0;i < files->Length;i++) {
+			if (files[i] == openFileDialog1->FileName) {
+				currentIndex = i;
+				break;
+			}
+		}
+	}
+}
+
+private: System::Void button5_Click(System::Object^  sender, System::EventArgs^  e) {
+	pictureBox1->Top -= zoomlevel / 2;
+	pictureBox1->Left -= zoomlevel / 2;
+	pictureBox1->Height += zoomlevel;
+	pictureBox1->Width += zoomlevel;
+	zooms++;
+
+}
+private: System::Void button7_Click(System::Object^  sender, System::EventArgs^  e) {
+	if (currentIndex != files->Length - 1) {
+		currentIndex++;
+	}
+	else currentIndex = 0;
+	pictureBox1->ImageLocation = files[currentIndex];
+	button8->PerformClick();
+}
+private: System::Void button2_Click(System::Object^  sender, System::EventArgs^  e) {
+	if (currentIndex != 0) {
+		currentIndex--;
+	}
+	else currentIndex = files->Length - 1;
+	pictureBox1->ImageLocation = files[currentIndex];
+	button8->PerformClick();
+}
+private: System::Void button6_Click(System::Object^  sender, System::EventArgs^  e) {
+	pictureBox1->Top += zoomlevel / 2;
+	pictureBox1->Left += zoomlevel / 2;
+	pictureBox1->Height -= zoomlevel;
+	pictureBox1->Width -= zoomlevel;
+	zooms--;
+
+}
+private: System::Void button8_Click(System::Object^  sender, System::EventArgs^  e) {
+	int newzoom = zooms;
+	if (zooms > 0) {
+		for (int i = 0;i < newzoom;i++) {
+			button6->PerformClick();
+		}
+	}
+	if (zooms < 0) {
+		for (int i = 0;i > newzoom;i--) {
+			button5->PerformClick();
+		}
+	}
+	if (rotates % 4 > 0) {
+		for (int i = 0;i < rotates%4;i++) {
+			button9->PerformClick();
+		}
+	}
+	rotates = 0;
+	zooms = 0;
+
+}
+private: System::Void button9_Click(System::Object^  sender, System::EventArgs^  e) {
+	
+		pictureBox1->Image->RotateFlip(RotateFlipType::Rotate90FlipXY);
+		rotates++;
+	
+}
+};
+}
